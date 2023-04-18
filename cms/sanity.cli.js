0f471db51c0ae32323330cd1ed9f5f5da77b9e52
Marion Rose <marionrose@Marions-MBP.attlocal.net>
2023-04-18 10:34:25 -0500

    switched sanity config file to js

--- cms/sanity.cli.js
+++ cms/sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '4h01pe1q',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
