45acfdea4a38fbb2833cfe2bc45de0a443743c68
Marion Rose <marionrose@Marions-MBP.attlocal.net>
2023-04-18 10:07:58 -0500

    testing vercel deploy after adding sanity cli

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,6 +36,7 @@ export default function Home({ aboutContent, newsAndEvents }: Props) {
       <Donate />
       {/* Contact Us */}
       <Contact />
+      testing
       {/* Footer */}
       <Footer />
     </>
